65378f2f1676ebbf5874b0db2434ed33a19ff5d5
gsanchez19 <42978523+gsanchez19@users.noreply.github.com>
2018-11-09 06:20:59 -0500

    Add files via upload

---
+++ GettysburgMain.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+import java.io.File;
+public class GettysburgMain {
+
+	public static void main(String []args)
+	{
+		Scanner file = new Scanner(new File("GettysburgAddress.txt"));
+		
+		
+		
+		
+		
+		
+		
+		
+	}
+
+}
