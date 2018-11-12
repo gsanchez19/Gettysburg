11c05113e51d9ed974d45e20488df66842f611b2
gsanchez19 <42978523+gsanchez19@users.noreply.github.com>
2018-11-11 20:06:36 -0500

    Add files via upload

--- GettysburgMain.java
+++ GettysburgMain.java
@@ -1,14 +1,73 @@
+import java.util.ArrayList;
 import java.util.Scanner;
 import java.io.File;
+import java.io.FileNotFoundException;
 public class GettysburgMain {
 
-	public static void main(String []args)
+	public static void main(String []args) throws FileNotFoundException
 	{
-		Scanner file = new Scanner(new File("GettysburgAddress.txt"));
+		File getty = new File("GettysburgAddress.txt");
+		Scanner file = new Scanner(getty);
+		String gettyFile = file.nextLine();
+		while(file.hasNextLine())
+		{
+			gettyFile = gettyFile + file.nextLine();
+		}
+	
 		
+		ArrayList<String> gettyRay = new ArrayList<String>(gettyFile.length());
+		// variables for characters that would normally split sentences or end them
+		String space = " ";
+		String period = ".";
+		String comma = ",";
+		String dash = "-";
 		
+		String word = "";
+		for(int i = 0; i < gettyFile.length(); i++)
+		{
+			// if the character at the index reaches the 
+			if(gettyFile.charAt(i) == space.charAt(0) || gettyFile.charAt(i) == period.charAt(0) || gettyFile.charAt(i) == comma.charAt(0) || gettyFile.charAt(i) == dash.charAt(0))
+			{
+				gettyRay.add(word);
+				word = "";
+			}
+			
+			else
+			{
+				word = "" + word + gettyFile.charAt(i);
+			}
+			
+			
+		}
 		
+		String longest = "";
+		// for(String gRWord : gettyRay)
+		for(int i = 0; i < gettyRay.size(); i++)
+		{
+			String gRWord = gettyRay.get(i);
+			if(longest.length() < gRWord.length())
+			{
+				longest = gRWord;
+			}
+			
+		}
 		
+		int avgLength = 0;
+		for(String gWord : gettyRay)
+		{
+			avgLength = avgLength + gWord.length();
+			
+		}
+		avgLength = avgLength / gettyRay.size();
+		
+		System.out.println("The longest word in the Gettysburg Address is: " + longest);
+		System.out.println("The average word length is: " + avgLength);
+		System.out.println(gettyFile.length());
+		System.out.println(gettyRay.size());
+		System.out.println("" + gettyRay.get(gettyRay.size()-1));
+		System.out.println(gettyRay.get(11));
+		
+		System.out.println(gettyFile);
 		
 		
 		
